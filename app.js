99e88728414fd8e11527444b60d421433afaa5d9
Taha Mousavi <tahamousavi6@gmail.com>
2024-01-11 19:47:39 +0330

    telegram api added

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-console.log("taha");
-console.log("taha1");
+const TelegramBot = require('node-telegram-bot-api');
+require("dotenv").config()
+
+const {telegram_token} = process.env;
+
+const bot = new TelegramBot(telegram_token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; 
+
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+    if(msg.text==="amir")
+        bot.sendMessage(chatId,'salam amir')
+});
